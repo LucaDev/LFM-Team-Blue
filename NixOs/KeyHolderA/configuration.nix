ddc69a4480ec2ce238b39dd2669d338bff224695
Florian <overflodo@web.de>
2026-04-26 21:49:51 +0200

    bootable

--- NixOs/KeyHolderA/configuration.nix
+++ NixOs/KeyHolderA/configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   imports = [    
     ./hardware-configuration.nix
     ./profiles/base.nix
