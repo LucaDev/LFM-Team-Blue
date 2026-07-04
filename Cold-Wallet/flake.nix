d8a39c05ec98f501bca40e14bb87f88699185b1f
overflowdo <flodomail@web.de>
2026-07-04 22:20:56 +0200

    flake cold fix

--- Cold-Wallet/flake.nix
+++ Cold-Wallet/flake.nix
@@ -3,21 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
+  outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    #pkgsUnstable = import nixpkgs-unstable { inherit system; };
   in
   {
     nixosConfigurations.cold = nixpkgs.lib.nixosSystem {
       inherit system;
-
-      #specialArgs = {
-      #  inherit pkgsUnstable;
-      #};
 
       modules = [
         ./configuration.nix
