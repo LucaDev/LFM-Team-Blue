86dfba7bff2f23608c7f45895b73de95204fb74d
Luca Bäcker <l.kroeger01@gmail.com>
2026-07-15 23:12:41 +0200

    fix wireguard connection

--- apphost/nixos/modules/wireguard.nix
+++ apphost/nixos/modules/wireguard.nix
@@ -2,7 +2,7 @@
 
 {
   networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.10.0.2/24" ];
+    ips = [ "10.10.0.1/24" ];
     listenPort = 51820;
 
     privateKeyFile = "/var/lib/wireguard/private.key";
@@ -12,7 +12,7 @@
         PEER_FILE="/var/lib/wireguard/signer.json"
         if [ -f "$PEER_FILE" ]; then
             PUBKEY=$(${pkgs.jq}/bin/jq -r '.signer_public_key' "$PEER_FILE")
-            ALLOWED=$(${pkgs.jq}/bin/jq -r '.allowed_ips_signer' "$PEER_FILE")
+            ALLOWED=$(${pkgs.jq}/bin/jq -r '.allowed_ips_wallet' "$PEER_FILE")
             ENDPOINT=$(${pkgs.jq}/bin/jq -r '.endpoint' "$PEER_FILE")
 
             ${pkgs.wireguard-tools}/bin/wg set wg0 \
